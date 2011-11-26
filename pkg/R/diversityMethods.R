b49ab5bbb43b71f8e66bced2f04b689928d02d88
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2011-11-26 20:07:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/landscape@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/diversityMethods.R
@@ -0,0 +1,48 @@
+# Author: Robert J. Hijmans
+# Date : November 2011
+# Version 1.0
+# Licence GPL v3
+
+
+if (!isGeneric("diversity")) {
+	setGeneric("diversity", function(x, y, ...)
+		standardGeneric("diversity"))
+}	
+
+
+.getDivFun <- function(fun) {
+	if (!is.function(fun)) {
+		fn <- tolower(fun[1])
+		if (fn == 'richness') {
+			fun <- function(x) length(unique(x))
+		} else if (fn == 'shannon') {
+			fun <- .shannon
+		} else if (fn == 'simpson') {
+			fun <- .simpson
+		} else if (fn == 'margalef ') {
+			fun <- .margalef 
+		}
+	}
+	fun
+}
+
+
+# diversity of raster cells for a given neighborhood defined by y (as in raster::aggregate)
+setMethod('diversity', signature(x='RasterLayer', y='numeric'), 
+function(x, y, fun='richness', ...) {
+	fun <- .getDivFun(fun)
+	aggregate(x, y, fun, ...)
+}
+)
+
+# diversity of raster cells based on point data
+setMethod('diversity', signature(x='SpatialPointsDataFrame', y='Raster'), 
+function(x, y, field=NULL, fun='richness', ...) {
+	if (is.null(field)) {
+		stop('you must provide an argument "field" to indicate\nthe variable in the SpatialPointsDataFrame that you want to use')
+	}
+	fun <- .getDivFun(fun)
+	rasterize(x, y, field=field, fun=fun, ...)
+}
+)
+
